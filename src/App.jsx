ab531e9580dc193b4749478eacc4e41be6e40cc9
ulemos <fsnvag12@gmail.com>
2024-04-09 05:00:23 -0300

    :memo: Filter and Buttons from complete and remove.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Todo from './components/Todo';
 import TodoForm from './components/TodoForm';
+import Search from "./components/Search";
 
 import {useState} from 'react';
 
@@ -27,6 +28,8 @@ const App = () => {
     }
   ])
 
+  const [search, setSearch] = useState("");
+
   const addTodo = (text, category) => {
     const newTodos = [...todos, {
       id: Math.floor(Math.random() * 10000),
@@ -34,15 +37,28 @@ const App = () => {
       category,
       isCompleted: false
     }]
-    setTodos(newTodos)
+    setTodos(newTodos);
+  }
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null);
+    setTodos(filteredTodos);
+  }
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo);
   }
 
   return (
     <div className='app'>
       <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch}/>
       <div className='todo-list'>
-        {todos.map((todo) => (
-          <Todo key={todo.id} todo = {todo}/>
+        {todos.filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).map((todo) => (
+          <Todo key={todo.id} todo = {todo} removeTodo = {removeTodo} 
+          completeTodo = {completeTodo}/>
         ))}
       </div>
       <TodoForm addTodo = {addTodo}/>
